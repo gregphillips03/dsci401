e0d656f27facf7411716845f54dbf60c4d3b2f70
Greg Phillips <greg.phillips@aecom.com>
2017-12-05 23:43:24 -0500

    initial push for final assignment

---
+++ phillips_assignment_4.py
@@ -0,0 +1,54 @@
+#Assignment #4
+#DSCI401B
+#William (Greg) Phillips
+#Working
+
+import pandas as pd
+import pprint
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import neighbors
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn import ensemble 
+from sklearn.metrics import classification_report
+
+# -------------------------------------- #
+# --- Section 0: Meta Data & Caveats --- #
+# -------------------------------------- #
+
+'''
+The following transformation were made to the data outside of the work enviornment:
+
+- The data set contained the column 'Date of Incident', which was renamed to 'Month'
+- Each record contained the specific calendar day which the incident occurred. These fields were mapped
+to the corresponding month of the year, as a text lable; i.e., 03/04/2017 became 'March'. 
+
+- Changed 'Worker Type' WHERE value == 'Contractor/Subcontractor (Managed)'
+- Replaced 1040 locations with 'Contractor'
+
+- Changed 'Worker Type' WHERE value == 'Contractor/Subcontractor (Unmanaged)'
+- Replaced 1316 locations with 'Contractor'
+
+The following records were removed from the data set:
+
+- The data set contains more than 'AECOM Employees' and 'Contractors'. It also contains joint venture
+partners, as well as members of the public. 
+
+- Removed records WHERE value of 'Worker Type' == 'JV Partner'. 
+- Removed a total of 6 records. 
+
+- Removed records WHERE value of 'Worker Type' == 'Third Party/Public'. 
+- Removed a total of 38 records. 
+
+
+
+
+
+
