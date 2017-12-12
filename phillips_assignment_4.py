baaf671e362d652bbb06b59c8e3e73f115e198e6
Greg Phillips <greg.phillips@aecom.com>
2017-12-11 19:14:35 -0500

    cleaned up some import statements

--- phillips_assignment_4.py
+++ phillips_assignment_4.py
@@ -7,20 +7,10 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-
-import pprint
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn import neighbors
-from sklearn import linear_model
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import confusion_matrix
-from sklearn import ensemble 
-from sklearn.metrics import classification_report
+from sklearn.cross_validation import KFold
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.neighbors import KNeighborsClassifier as KNN
 
 # -------------------------------------- #
 # --- Section 0: Meta Data & Caveats --- #
